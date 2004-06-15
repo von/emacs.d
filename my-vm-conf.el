92008569ca975bc0196b02bf6ec05a0659fdcf0f
welch <welch@fa4a8205-1563-0410-ac01-9a61a5fdf168>
2004-06-15 23:09:42 +0000

    Changed default address to vwelch@ncsa.uiuc.edu Changed to using fetchmail Added personal email to esmtpmail configuration Changed default for saving mime attachments to documents directory

--- my-vm-conf.el
+++ my-vm-conf.el
@@ -5,14 +5,16 @@
 ;; my email environment.
 ;;
 
+(require 'my-setup)
+
 ;; Set my mailing address
 (setq-default user-full-name "Von Welch")
-(setq-default user-mail-address "welch@mcs.anl.gov")
+(setq-default user-mail-address "vwelch@ncsa.uiuc.edu")
 
 ;; Needed for feedmail and smtpmail to work together
 (setq-default mail-envelope-from user-mail-address)
 
-;; Regex of senders for whom I want to receiver to appear in the
+;; Regex of senders for whom I want receiver to appear in the
 ;; summary instead of the sender
 (setq vm-summary-uninteresting-senders
       "\\(vwelch@ncsa.uiuc.edu\\|welch@mcs.anl.gov\\|von@vwelch.com\\)")
@@ -25,14 +27,42 @@
 		"von@vwelch.com"
 		))
 
-;;(setq vm-spool-files '("localhost:110:pass:welch:*"))
+;; Used to set file vm opens by default
+(setq vm-primary-inbox "~/Mail/mbox")
+
 (setq vm-spool-files
       '(
-	("mbox" "imap:localhost:143:inbox:login:welch:*" "inbox.crash")
-	("ncsa" "pop:pop.ncsa.uiuc.edu:110:apop:vwelch:*" "ncsa.crash")
+	("mbox" "inbox" "mbox.crash")
+	;;("mcs" "imap:localhost:10143:inbox:login:welch:*" "mcs.crash")
+	;;("ncsa" "pop:pop.ncsa.uiuc.edu:110:apop:vwelch:*" "ncsa.crash")
 	))
 
 (setq smtpmail-smtp-server "localhost")
 (setq esmtpmail-smtp-server smtpmail-smtp-server)
+
+;; Reply-to's to ignore
+;; XXX Change this to ignore all!
+(setq vm-reply-ignored-reply-tos 
+      '(
+	"ogsi-wg@gridforum.org"
+	"secres-leadership@ncsa.uiuc.edgu"
+	"security-wg@teragrid.org"
+	))
+
+(setq esmtpmail-send-it-by-alist
+      ;; Format: (sender smtp-host) or (sender (smtp-host port))
+      ;; Note that if I am resending, the sending will show up as the
+      ;; original sender and not me.
+      '(
+	("welch@mcs.anl.gov" ("localhost" 10025))
+	("von@vwelch.com" ("localhost" 12025))
+	;; Default, includes NCSA.
+	(t "smtp.ncsa.uiuc.edu")
+	;; For when I have to tunnel
+	(t ("localhost" 11025))
+	))
+
+;; Default place to save attachments
+(setq-default vm-mime-attachment-save-directory my-documents-dir)
 
 (provide 'my-vm-conf)

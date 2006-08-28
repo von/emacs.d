5a3d47ed61517fc4aba9fd817277bdf62ca49231
welch <welch@fa4a8205-1563-0410-ac01-9a61a5fdf168>
2006-08-28 21:59:57 +0000

    Added my-java-mode-init Commented out compilation-regex-alist code that was causing problems on Mac

--- my-java.el
+++ my-java.el
@@ -7,20 +7,38 @@
 
 (require 'compile)
 
-;; Javac
-;; <file>:<line no>: <error text>
-(setq javac-compilation-regexp-alist
-      '(("\\([^ ]+\\):\\([0-9]+\\):" 1 2)))
+(defun my-java-mode-init()
+  "My Java mode init"
 
-(cond ((boundp 'compilation-error-regexp-alist-alist)
-       (setq compilation-error-regexp-alist-alist
-	     (cons (cons 'javac javac-compilation-regexp-alist)
-			   compilation-error-regexp-alist-alist))
-       (compilation-build-compilation-error-regexp-alist))
+  (make-local-variable 'tab-width)
+  (setq
 
-      ((boundp 'compilation-error-regexp-alist)
-       (setq compilation-error-regexp-alist
-	     (cons javac-compilation-regexp-alist
-		   compilation-error-regexp-alist))
-       )
-      )
+   ;; Make tabs always 4
+   tab-width 4
+
+   ;; Don't use tabs for indentation
+   indent-tabs-mode nil
+   )
+)
+
+(add-hook 'java-mode-hook 'my-java-mode-init)
+
+;; XXX MAC OS X 10.4 issues
+;; ;; Javac
+;; ;; <file>:<line no>: <error text>
+;; (setq javac-compilation-regexp-alist
+;;       '(("\\([^ ]+\\):\\([0-9]+\\):" 1 2)))
+
+;; (cond ((and (boundp 'compilation-error-regexp-alist-alist)
+;; 	    (functionp 'compilation-build-compliation-error-regexp-alist))
+;;        (setq compilation-error-regexp-alist-alist
+;; 	     (cons (cons 'javac javac-compilation-regexp-alist)
+;; 			   compilation-error-regexp-alist-alist))
+;;        (compilation-build-compilation-error-regexp-alist))
+
+;;       ((boundp 'compilation-error-regexp-alist)
+;;        (setq compilation-error-regexp-alist
+;; 	     (cons javac-compilation-regexp-alist
+;; 		   compilation-error-regexp-alist))
+;;        )
+;;       )

cacef6c4e9ccaff4753f4d6b947638323c8c9bda
welch <welch@fa4a8205-1563-0410-ac01-9a61a5fdf168>
2006-08-28 21:56:47 +0000

    Commented out auto-loading of files on startup

--- my-setup.el
+++ my-setup.el
@@ -10,9 +10,12 @@
 (setq my-documents-dir
       (concat 
        (cond
-	;; Check for ~/My Documents
+	;; Check for ~/My Documents (Windows)
 	((file-exists-p (concat home directory-sep-string "My Documents"))
 	 (concat home directory-sep-string "My Documents"))
+	;; Check for ~/Documents (Mac OS)
+	((file-exists-p (concat home directory-sep-string "Documents"))
+	 (concat home directory-sep-string "Documents"))
 	;; Fall back to home directory
 	(t home)
 	)
@@ -21,22 +24,22 @@
       )
 
 ;; Fire off a new frame with windows for Todo, Dates and diary.
-(if (file-exists-p (concat my-documents-dir "Todo.txt"))
-    (progn
-      (select-frame (make-frame '(height 55)))
-      (find-file (concat my-documents-dir "Todo.txt"))
-      ;;
-      ;; Dates pane
-      ;; (Split at 1/3 total size for 3 panes)
-      (select-window (split-window-vertically (/ (frame-height) 3)))
-      (find-file (concat my-documents-dir "dates.txt"))
-      (select-window (split-window-vertically))
-      ;;
-      ;; Diary pane
-      (require 'vdiary-mode)
-      (find-file (concat my-documents-dir "diary.txt"))
-      )
-)
+;; (if (file-exists-p (concat my-documents-dir "Todo.txt"))
+;;     (progn
+;;       (select-frame (make-frame '(height 55)))
+;;       (find-file (concat my-documents-dir "Todo.txt"))
+;;       ;;
+;;       ;; Dates pane
+;;       ;; (Split at 1/3 total size for 3 panes)
+;;       (select-window (split-window-vertically (/ (frame-height) 3)))
+;;       (find-file (concat my-documents-dir "dates.txt"))
+;;       (select-window (split-window-vertically))
+;;       ;;
+;;       ;; Diary pane
+;;       (require 'vdiary-mode)
+;;       (find-file (concat my-documents-dir "diary.txt"))
+;;       )
+;; )
 
 (message "Von's setup done")
 (provide 'my-setup)

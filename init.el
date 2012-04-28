83e81681e97dc1b74bc74128593b797da624c2d9
Von Welch <von@vwelch.com>
2012-04-28 19:54:17 -0400

    init.el: set markdown-command to Markdown.pl

--- init.el
+++ init.el
@@ -214,7 +214,7 @@
   "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
-
+(set-variable 'markdown-command "Markdown.pl")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;

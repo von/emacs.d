feeff6f5a3d7d8976b55b7116feeaaf2e2197dcf
Von Welch <von@vwelch.com>
2012-05-18 07:23:26 -0400

    Added some whitespace for readability.

--- init.el
+++ init.el
@@ -251,15 +251,18 @@
 
 ;;* flyspell comments and strings in programming modes
 ;; From: "Stefan Monnier <foo @ acm.com>"
+
 (defun flyspell-generic-progmode-verify ()
    "Used for `flyspell-generic-check-word-p' in programming modes."
    (let ((f (get-text-property (point) 'face)))
      (memq f '(font-lock-comment-face font-lock-string-face))))
+
 (defun flyspell-prog-mode ()
    "Turn on `flyspell-mode' for comments and strings."
    (interactive)
    (setq flyspell-generic-check-word-p 'flyspell-generic-progmode-verify)
    (flyspell-mode 1))
+
 (add-hook 'c-mode-common-hook         'flyspell-prog-mode t)
 (add-hook 'java-mode-common-hook         'flyspell-prog-mode t)
 

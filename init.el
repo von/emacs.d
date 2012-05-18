466dc5ae5717f9a213f4fb3b20557db16e3a511a
Von Welch <von@vwelch.com>
2012-05-18 11:19:20 -0400

    Use nxhtml

--- init.el
+++ init.el
@@ -412,6 +412,16 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; Load nxhtml mode
+
+(load "nxhtml/autostart.el")
+
+;; Stop nxhtml from messing with my background
+;; Kudos: http://stackoverflow.com/a/3042760/197789
+(setq mumamo-background-colors nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;; End entering debugger on any error in this file
 ;;
 

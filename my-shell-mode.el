17d9923ee51439c503282fef8db1186ae3d239c8
Von Welch <von@vwelch.com>
2012-05-23 18:16:22 -0400

    my-shell-mode.el: Make control-u delete current input.

--- my-shell-mode.el
+++ my-shell-mode.el
@@ -21,8 +21,9 @@
       (comint-next-input arg)
     (next-line arg)))
 
-;; bind my special functions to the up and down keys in shell-mode
 (add-hook 'shell-mode-hook
           (lambda ()
+	    (define-key shell-mode-map "C-u" 'comint-delete-input)
+	    ;; bind my special functions to the up and down keys in shell-mode
             (define-key shell-mode-map [up] 'ewd-comint-up)
             (define-key shell-mode-map [down] 'ewd-comint-down)))

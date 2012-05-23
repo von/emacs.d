a725cdde93072ef3e1ebcf5b3bec641188ccda6d
Von Welch <von@vwelch.com>
2012-05-23 17:05:02 -0400

    Don't ask about killing processes when exiting.

--- init.el
+++ init.el
@@ -427,6 +427,15 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; Don't ask about killing process on exit
+;; Kudos: http://stackoverflow.com/a/2706660/197789
+
+(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
+  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
+  (flet ((process-list ())) ad-do-it))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;; End entering debugger on any error in this file
 ;;
 

2a834b6dddded3f5d67f3341365521ee10ba6248
Von Welch <von@vwelch.com>
2012-05-23 18:12:57 -0400

    Added my-shell-mode.el with fix for arrow keys.

---
+++ my-shell-mode.el
@@ -0,0 +1,28 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Make arrow keys work as I would expect in shell-mode
+;; Kudos: Eli Daniel
+;;   http://lists.gnu.org/archive/html/help-emacs-windows/2002-10/msg00059.html
+
+;; move cursor to the previous line or get previous history item, depending
+;; on whether we're at a shell mode prompt
+
+(defun ewd-comint-up (arg)
+  (interactive "p")
+  (if (comint-after-pmark-p)
+      (comint-previous-input arg)
+    (previous-line arg)))
+
+;; move cursor to the next line or get next history item, depending
+;; on whether we're at a shell mode prompt
+(defun ewd-comint-down (arg)
+  (interactive "p")
+  (if (comint-after-pmark-p)
+      (comint-next-input arg)
+    (next-line arg)))
+
+;; bind my special functions to the up and down keys in shell-mode
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (define-key shell-mode-map [up] 'ewd-comint-up)
+            (define-key shell-mode-map [down] 'ewd-comint-down)))

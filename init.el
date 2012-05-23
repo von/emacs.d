bbc46a5aaa05f0f62b94e4729abcbbbb4c438dfa
Von Welch <von@vwelch.com>
2012-05-23 16:58:40 -0400

    Enabled desktop-save mode to save all my open files when exiting.

--- init.el
+++ init.el
@@ -198,12 +198,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Desktop saving configuration
+;;
+;; Saves all the open files when exiting for restoration on next start.
+
+;; Turn on desktop saving
+(desktop-save-mode t)
+
+;; Always save, don't ask
+(setq desktop-save t)
 
 ;; Where to save desktop file
 (setq desktop-dirname home)
-
-;; Turn on desktop saving
-;;(desktop-save-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;

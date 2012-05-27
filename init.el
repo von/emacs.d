dc3d8e592a1d7ceac8d31bfae2c75702071206c9
Von Welch <von@vwelch.com>
2012-05-27 16:00:22 -0400

    Created setup-load-path.el

--- init.el
+++ init.el
@@ -16,53 +16,8 @@
 
 (setq debug-on-error t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Set up Emac's load path
-;;
-;; See: http://emacswiki.org/emacs/LoadPath
-
-;; Use directory-file-name here to strip any trailing directory
-;; separator
-(defvar home (directory-file-name (getenv "HOME"))
-  "My home directory.")
-
-;; Prepend load stuff...
-(setq directory-sep-string (char-to-string directory-sep-char))
-
-(setq my-lib-dir (concat home directory-sep-string "lib"))
-
-(setq my-emacs-config-dir (concat home
-				  directory-sep-string
-				  "emacs-config"
-				  directory-sep-string))
-(if (file-accessible-directory-p my-emacs-config-dir)
-     (setq load-path (cons my-emacs-config-dir load-path))
-)
-
-(setq my-lisp-dir (concat my-lib-dir directory-sep-string "lisp"))
-(if (file-accessible-directory-p my-lisp-dir)
-    (setq load-path (cons my-lisp-dir load-path))
-)
-
-;; Add all subdirectories of my-lisp-dir to my load path
-(let ((default-directory my-lisp-dir))
-      (normal-top-level-add-subdirs-to-load-path))
-
-;; Add lib/emacs-<majorversion>
-(setq my-emacs-dir (concat my-lib-dir directory-sep-string
-			   "emacs-" (int-to-string emacs-major-version)))
-(if (file-accessible-directory-p my-emacs-dir)
-    (progn
-      (setq load-path (cons my-emacs-dir load-path))
-      (let ((default-directory my-emacs-dir))
-	(normal-top-level-add-subdirs-to-load-path))
-      ))
-
-;; Add /usr/local/share/emacs/site-lisp/ if it exists
-(if (file-accessible-directory-p "/usr/local/share/emacs/site-lisp/")
-    (setq load-path (cons "/usr/local/share/emacs/site-lisp/" load-path))
-)
+;; Set up my load-path
+(load (expand-file-name "~/emacs-config/setup-load-path"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;

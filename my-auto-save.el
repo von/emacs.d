40103779d0b85b603858088c496b1a8a3a3c9909
Von Welch <von@vwelch.com>
2012-06-02 14:27:45 -0400

    my-auto-save.el: White space and comment clean up.

--- my-auto-save.el
+++ my-auto-save.el
@@ -1,34 +1,30 @@
 ;;
 ;; my-auto-save.el
 ;;
-;; XXX Has user-specifc paths
-;;
-;; $Id$
-;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (if is-xemacs
     (progn
       (setq auto-save-directory (if is-ms-windows
-				    (expand-file-name "c:/temp")
-				  (expand-file-name "/tmp/vwelch-autosaves/")
-				  )
-	    auto-save-directory-fallback auto-save-directory
-	    auto-save-hash-p nil
+                                    (expand-file-name "c:/temp")
+                                  (expand-file-name "/tmp/vwelch-autosaves/")
+                                  )
+            auto-save-directory-fallback auto-save-directory
+            auto-save-hash-p nil
 
-	    auto-save-offer-delete t
+            auto-save-offer-delete t
 
-	    ;; Don't put save lists in ~ to reduce clutter
-	    auto-save-list-file-prefix "~/.autosave/saves-"
+            ;; Don't put save lists in ~ to reduce clutter
+            auto-save-list-file-prefix "~/.autosave/saves-"
 
-	    ange-ftp-auto-save t
-	    ange-ftp-auto-save-remotely nil
-	    ;; Keystrokes between autosaves
-	    auto-save-interval 2000
+            ange-ftp-auto-save t
+            ange-ftp-auto-save-remotely nil
+            ;; Keystrokes between autosaves
+            auto-save-interval 2000
 
-	    ;; Seconds between autosaves
-	    auto-save-timeout 300
-	    )
+            ;; Seconds between autosaves
+            auto-save-timeout 300
+            )
       (require 'auto-save)
       )
 )

c3b35dfe2392276d2a7dc3fcc2270a089d7b0000
Von Welch <von@vwelch.com>
2012-06-05 19:27:33 -0400

    my-markdown.el: Added comment with Markdown.pl URL

--- my-markdown.el
+++ my-markdown.el
@@ -1,6 +1,7 @@
 ;;;
 ;;; Load and configure Markdown mode
 ;;;
+;;; Markdown is available from: http://daringfireball.net/projects/markdown/
 
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)

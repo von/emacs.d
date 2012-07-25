ea22eeb88160c8536be2cfd46110c8a32b9d4c87
Von Welch <von@vwelch.com>
2012-07-24 20:47:08 -0400

    Added my-rst.el: restructured text mode configuration

---
+++ my-rst.el
@@ -0,0 +1,23 @@
+;;;
+;;; My rst (restructed text) mode configuration
+;;;
+
+;; Get rid of grey background and use bright foreground instead
+(set-face-background 'rst-level-1-face "black")
+(set-face-foreground 'rst-level-1-face "red")
+
+(set-face-background 'rst-level-2-face "black")
+(set-face-foreground 'rst-level-2-face "orange")
+
+(set-face-background 'rst-level-3-face "black")
+(set-face-foreground 'rst-level-3-face "yellow")
+
+(set-face-background 'rst-level-4-face "black")
+(set-face-foreground 'rst-level-4-face "yellow")
+
+(set-face-background 'rst-level-5-face "black")
+(set-face-foreground 'rst-level-5-face "yellow")
+
+(set-face-background 'rst-level-6-face "black")
+(set-face-foreground 'rst-level-6-face "yellow")
+
